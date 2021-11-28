923bd9b97875bb25a58280aec177d81611849b35
Peter Easter, DO, MBA/MHA, FAAP <53067516+p-easter@users.noreply.github.com>
2021-11-28 14:45:26 -0600

    Add files via upload

---
+++ Quotes.py
@@ -0,0 +1,33 @@
+import streamlit as st
+import pandas as pd
+import random as ran
+
+st.title('Find Your Perfect Quote!')
+st.markdown('### You can either get a random quote:')
+file = '~/Documents/Programming/Quotes/Quotes_lg.csv'
+
+@st.cache
+def getData(file):
+	df = pd.read_csv(file)
+	df.drop('Unnamed: 0', axis=1, inplace=True)
+	return df
+
+def randomQuote(df):
+	x = ran.randint(0,len(df))
+	quote = df.iloc[x]
+	return "{} \n\n ~ {}".format(quote['quotes'],quote['author'])
+
+def getQuotes(df, option):
+	quotes = df[df['author'] == option]['quotes']
+	for q in quotes:
+		st.write(q)
+
+df = getData(file)
+
+st.button('Get Random Quote', on_click=st.write(randomQuote(df)))
+
+st.markdown('### Or you can select based on the author:')
+option = st.selectbox(label = '',
+	options = pd.Series(df['author'].unique()))
+
+st.write(getQuotes(df, option))
